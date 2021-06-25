d36cffdd0749aa066adf454f5391214f8fd695d4
GitHub Action <githubactions@yuste.com>
2021-06-25 15:16:06 +0000

    Update GitHub pages

--- src/components/ErrorMessage/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorMessage = () => {
-  return <p>An error has occurred</p>;
-};
-
-export default ErrorMessage;
--- src/utils/display/index.js
+++
@@ -1,9 +0,0 @@
-export const INTEVAL_SCREEN_TIME = 10000;
-
-export const enterFullScreen = () => {
-  !document.fullscreenElement && document.firstElementChild.requestFullscreen();
-};
-
-export const exitFullScreen = () => {
-  document.fullscreenElement && document.exitFullscreen();
-};
